c7b509737aea2800144a1443d79a7e4b46ecde60
121167390@qq.com <121167290@qq.com>
2018-09-27 23:52:45 +0800

    add func

---
+++ func.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 函数代码块以 def 关键词开头，后接函数标识符名称和圆括号()。
+# 任何传入参数和自变量必须放在圆括号中间。圆括号之间可以用于定义参数。
+# 函数的第一行语句可以选择性地使用文档字符串—用于存放函数说明。
+# 函数内容以冒号起始，并且缩进。
+# return [表达式] 结束函数，选择性地返回一个值给调用方。不带表达式的return相当于返回 None。
+
+# python 函数的参数传递：
+# 不可变类型：类似 c++ 的值传递，如 整数、字符串、元组。如fun（a），传递的只是a的值，没有影响a对象本身。比如在 fun（a）内部修改 a 的值，只是修改另一个复制的对象，不会影响 a 本身。
+# 可变类型：类似 c++ 的引用传递，如 列表，字典。如 fun（la），则是将 la 真正的传过去，修改后fun外部的la也会受影响
+# python 中一切都是对象，严格意义我们不能说值传递还是引用传递，我们应该说传不可变对象和传可变对象。
+
+def printme( str ):
+   "打印传入的字符串到标准显示设备上"
+   print "printme", str
+   return
+
+printme("HHHHHHHHHHHHHH")
+
+def printinfo( name, age ):
+   "打印任何传入的字符串"
+   print "Name: ", name;
+   print "Age ", age;
+   return;
+ 
+#调用printinfo函数
+printinfo( age=50, name="miki" );
+
+#可写函数说明
+def printinfo( name, age = 35 ):
+   "打印任何传入的字符串"
+   print "Name: ", name;
+   print "Age ", age;
+   return;
+ 
+#调用printinfo函数
+printinfo( age=50, name="miki" );
+printinfo( name="miki" );
+
+
+# 不定长参数
+def printinfo( arg1, *vartuple ):
+   "打印任何传入的参数"
+   print "输出: "
+   print arg1
+   for var in vartuple:
+      print var
+   return;
+ 
+# 调用printinfo 函数
+printinfo( 10 );
+printinfo( 70, 60, 50 );
+
+# 匿名函数
+# python 使用 lambda 来创建匿名函数。
+sum = lambda arg1, arg2: arg1 + arg2;
+ 
+# 调用sum函数
+print "相加后的值为 : ", sum( 10, 20 )
+print "相加后的值为 : ", sum( 20, 20 )
